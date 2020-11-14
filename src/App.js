bfe07c7dc9c9ccbad6acfdf01e7aacb93e8802ff
saifali99 <saifaly.jariya@gmail.com>
2020-11-14 15:51:22 +0500

    App Design

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
